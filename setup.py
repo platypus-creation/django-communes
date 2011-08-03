bede0719bbf6e63312044c17de37f6496f823bd0
Guillaume Esquevin <guillaume.esquevin@platypus-creation.com>
2011-08-03 10:45:07 +0200

    fixed settings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Communes",
+    name = "django-communes",
     version = "0.1.0",
     description = """
     A django app with the list of all French Communes with Postal code, Insee Code, Population, Latitude and Longitude
